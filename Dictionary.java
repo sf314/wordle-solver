ed30ca87b365065b42c11f5a26ac2ce0b0365a4f
Stephen Flores <sf314@icloud.com>
2022-03-05 09:52:29 -0600

    feature: Add initial source files
    - Solver
    - Dictionary

---
+++ Dictionary.java
@@ -0,0 +1,40 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class Dictionary {
+
+    // Props
+    private List<String> fullWordList;
+    private List<String> filteredList;
+    private Map<Character, Float> letterFrequency = new HashMap<>();
+
+    // Init
+    public Dictionary() {
+
+    }
+
+    // Funcs
+    public void loadFrom(String filePath) {
+        List<String> lines;
+        try {
+            Stream<String> stringStream = Files.lines(Paths.get("dictionary.txt"));
+            lines = stringStream.toList();
+            stringStream.close(); // Required per Closable iface
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load dictionary: " + e.getMessage(), e);
+        }
+
+        List<String> fullWordList = new ArrayList<>();
+        for (String l : lines) {
+            fullWordList.add(l);
+        }
+
+        // Make second copy of word list for filtering
+        filteredList = new ArrayList<>(fullWordList);
+    }
+}
---
+++ Solver.java
@@ -0,0 +1,133 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Solver {
+    private static String DICT_FILE = "dictionary.txt";
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Wordle Solver");
+
+        // Read dictionary file into list
+        List<String> lines;
+        try {
+            Stream<String> stringStream = Files.lines(Paths.get("dictionary.txt"));
+            lines = stringStream.toList();
+            stringStream.close(); // Required per Closable iface
+        } catch (Exception e) {
+            System.out.println("Failed to load dictionary!");
+            throw e;
+        }
+
+        List<String> dictionary = new ArrayList<>();
+        for (String l : lines) {
+            dictionary.add(l);
+        }
+        List<String> dictionary2 = new ArrayList<>(dictionary); // Clone
+        dictionary2.removeIf((word) -> {
+            return word.contains("a");
+        });
+
+        // Validate dictionary
+        System.out.println("Dictionary is of size: " + dictionary.size());
+        for (String word : dictionary) {
+            if (word.length() != 5) {
+                System.out.println("Word " + word + " is not 5 letters long!");
+            }
+        }
+
+        // Continually read input. Format: {guess} {result}
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("Command: guess, exit");
+            String command = scanner.nextLine();
+            System.out.println("Will " + command);
+            if (command.equals("exit")) {
+                break;
+            }
+
+            if (!command.equals("guess")) {
+                continue;
+            }
+
+            System.out.println("Guess the first word. Provide results as [b]lack, [y]ellow, or [green] for each letter.");
+            System.out.println("Note: if a repeated letter is both [b] and [y/g], then mark [b] instances as [y].");
+            String input = scanner.nextLine();
+            if (input.length() != 11) {
+                System.out.println("MUST PROVIDE GUESS AND RESULTS (11 chars)");
+                continue;
+            }
+
+            String guess = input.split(" ")[0];
+            String result = input.split(" ")[1];
+            System.out.println("Guess: " + guess);
+            System.out.println("Result: " + result);
+
+            // VALIDATION
+            if (guess.length() != 5) {
+                System.out.println("Guess needs to be 5 letters!");
+                continue;
+            }
+
+            if (result.length() != 5) {
+                System.out.println("Result needs to be 5 letters!");
+                continue;
+            }
+
+            if (!lines.contains(guess)) {
+                System.out.println("Guess needs to be a valid word!");
+                continue;
+            }
+
+            for (int i = 0; i < 5; i++) {
+                char ch = guess.charAt(i);
+                char charResult = result.charAt(i);
+
+                if (charResult == 'b') {
+                    System.out.println("Eliminating all words with " + ch);
+                    // Remove all words containing x
+                    dictionary.removeIf((word) -> {
+                        return word.contains(String.valueOf(ch));
+                    });
+                } else if (charResult == 'y') {
+                    System.out.println("Eliminating all words without " + ch);
+                    // Remove all words that don't contain x at all
+                    dictionary.removeIf((word) -> {
+                        return !word.contains(String.valueOf(ch));
+                    });
+
+                    System.out.println("Eliminating all words with " + ch + " at index " + i);
+                    // Remove all words that contain x at this specific index
+                    final int idex = i; // Required due to lambda grrrr
+                    dictionary.removeIf((word) -> {
+                        return word.charAt(idex) == ch;
+                    });
+                } else if (charResult == 'g') {
+                    System.out.println("Eliminating all words without " + ch + " at index " + i);
+                    // Remove all words that don't have x at this specific index
+                    final int idex = i;
+                    dictionary.removeIf((word) -> {
+                        return word.charAt(idex) != ch;
+                    });
+                } else {
+                    System.out.println("Invalid result char " + charResult);
+                }
+            }
+
+            // Print prelim results
+            System.out.println("New dictionary size: " + dictionary.size());
+            System.out.println("Top words: ");
+            for (int i = 0; i < (dictionary.size() > 50 ? 10 : dictionary.size()); i++) {
+                System.out.println("  " + dictionary.get(i));
+            }
+
+        }
+        scanner.close();
+
+        System.out.println("Thanks for playing!");
+    }
+}
