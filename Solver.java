2c3ad2bf72e801bab997f19443aa12283cf4fa54
Stephen Flores <sf314@icloud.com>
2022-03-06 16:11:01 -0600

    fix: Remove unused imports

--- Solver.java
+++ Solver.java
@@ -1,12 +1,7 @@
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Solver {
-    private static String DICT_FILE = "dictionary.txt";
 
     public static void main(String[] args) throws Exception {
         System.out.println("Wordle Solver");
